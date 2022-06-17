e6a448ebc942afb21dc32ef7a0f5256e2ac22e8e
Howard Jiang <jiangh77@gmail.com>
2022-06-17 07:10:31 -0600

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 18:05:48 2022
+
+@author: jiang
+"""
+
+# save these code in a file called mymodule.py
+
+def greeting(name):
+    print("Hello, " + name)
+
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
